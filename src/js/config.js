b8f68bed5c3faa1af62bbce25dc44cd0ba7b36f2
Hacklie <hacklypselon@gmail.com>
2024-03-12 13:57:59 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = `7917e4bb-50c3-46bf-b851-791b7abd35d5`;
+export const MODAL_CLOSE_SEC = 2.5;
