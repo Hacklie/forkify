b8711c8937d4c4143680e820e4eeed9fe60639d7
Hacklie <hacklypselon@gmail.com>
2024-04-07 00:50:48 +0200

    Edited fractional numbers in recipeView

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,8 @@ import View from './View.js';
 
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-import { Fraction } from 'fractional';
+// import { Fraction } from 'fractional';
+import { number2fraction } from 'number2fraction';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -133,7 +134,8 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ``
+        // ing.quantity ? new Fraction(ing.quantity).toString() : ``
+        ing.quantity ? number2fraction(ing.quantity).toString() : ``
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
